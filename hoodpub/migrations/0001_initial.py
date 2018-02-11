faea6a11b52956c44cafc3edaf5eac2b585c4fec
Hyunmin Choi <pointer81@gmail.com>
2018-02-11 20:45:05 +0900

    Django to downgrade to 1.11

--- hoodpub/migrations/0001_initial.py
+++ hoodpub/migrations/0001_initial.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('auth', '0009_alter_user_last_name_max_length'),
+        ('auth', '0008_alter_user_username_max_length'),
     ]
 
     operations = [
--- hoodpub/views.py
+++ hoodpub/views.py
@@ -1,9 +1,10 @@
-import requests
 from django.db.models import Count
 from rest_framework import status
 from rest_framework.decorators import list_route
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+
+from daumapi.search import Search
 
 from .models import Book, User
 from .serializers import UserSerializer
@@ -25,24 +26,14 @@ class UserBookViewSet(ModelViewSet):
     def create(self, request, *args, **kwargs):
         keyword = request.data.get('keyword', '자전거')
 
-        payload = {
-            "output": "json",
-            "result": 20,
-            "apikey": "9f68e425f40e190b407745eb855619262ce0b2cc",
-            "searchType": "title"
-        }
+        search = Search()
+        res = search.request(keyword=keyword)
 
-        if keyword:
-            payload['q'] = keyword
-
-        url = 'https://apis.daum.net/search/book'
-        res = requests.get(url, params=payload)
-
-        for ele in res.json()['channel']['item']:
+        for ele in res['items']:
             book, created = Book.objects.update_or_create(
                 barcode=ele['barcode'], defaults=ele)
-            print(ele)
-        return Response(res.json())
+
+        return Response(res)
 
     @list_route(methods=['post'])
     def add_user(self, request, *args, **kwargs):
